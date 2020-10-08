4e828d80657ded16bd0bbd3dacebed08a5231677
Psilo <psilo@unterrainer.info>
2020-10-08 15:27:41 +0200

    try to add keycloak

--- set_deployment_env.sh
+++ set_deployment_env.sh
@@ -6,3 +6,7 @@ echo "VUE_APP_TITLE=Nexus" >> .env
 echo "VUE_APP_PROTOCOL=https" >> .env
 echo "VUE_APP_ADDRESS=nexus-server.unterrainer.info" >> .env
 echo "VUE_APP_PORT=443" >> .env
+
+echo "KEYCLOAK_PASSWORD=$KEYCLOAK_PASSWORD" >> .env
+echo "KEYCLOAK_DB_PASSWORD=$KEYCLOAK_DB_PASSWORD" >> .env
+echo "KEYCLOAK_DB_ROOT_PASSWORD=$KEYCLOAK_DB_ROOT_PASSWORD" >> .env
