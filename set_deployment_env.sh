e441b99d8cf42b9ac8a09d42026967674b1eba9b
Gerald <gu_falcon@hotmail.com>
2020-10-08 13:40:13 +0200

    Update set_deployment_env.sh

--- set_deployment_env.sh
+++ set_deployment_env.sh
@@ -2,3 +2,6 @@
 
 ## This file will be used in the docker-compose.yml file automatically because of its name and location.
 ## So this is the place where to transfer the CI-variables to docker-compose.
+VUE_APP_PROTOCOL=https
+VUE_APP_ADDRESS=nexus-server.unterrainer.info
+VUE_APP_PORT=443
