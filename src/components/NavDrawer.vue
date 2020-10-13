d898146e1a2259b20e1ccc4082ff119211da3fe0
Psilo <psilo@unterrainer.info>
2020-10-13 15:47:55 +0200

    add vueUtils with a utility method

--- src/components/NavDrawer.vue
+++ src/components/NavDrawer.vue
@@ -1,12 +1,28 @@
 <template>
   <v-container>
     <div v-for="(item, i) in mnu" :key="i">
-      <v-list nav shaped class="ma-0 pa-0">
-        <v-list-item v-if="item.link === 'SPACER'"> </v-list-item>
+      <v-list
+        nav
+        v-if="item.role == null || keycloakClientRoles.includes(item.role)"
+      >
+        <v-list-item
+          v-if="item.link === 'SPACER'"
+          dense
+          class="ma-0 pa-0"
+        ></v-list-item>
         <div v-if="item.link !== 'SPACER'">
           <v-tooltip top :open-delay="openDelay" :disabled="!tooltips">
             <template v-slot:activator="{ on, attrs }">
-              <v-list-item class="accent elevation-4" v-bind="attrs" v-on="on">
+              <v-list-item
+                :class="
+                  'elevation-4 ' + item.color == null ||
+                  item.color === undefined
+                    ? 'accent'
+                    : item.color
+                "
+                v-bind="attrs"
+                v-on="on"
+              >
                 <v-list-item-content>
                   <v-list-item-title class="title">
                     {{ $t(item.key) }}
@@ -20,33 +36,43 @@
         </div>
 
         <div v-for="(subItem, j) in item.subs" :key="j">
-          <v-list-item v-if="subItem.link === 'SPACER'"> </v-list-item>
-          <v-tooltip
-            v-if="subItem.link !== 'SPACER'"
-            top
-            :open-delay="openDelay"
-            :disabled="!tooltips"
+          <v-list-item
+            v-if="subItem.link === 'SPACER'"
+            dense
+            class="ma-0 pa-0"
+          ></v-list-item>
+          <div
+            v-if="
+              subItem.role == null || keycloakClientRoles.includes(subItem.role)
+            "
           >
-            <template v-slot:activator="{ on, attrs }">
-              <v-list-item
-                :to="subItem.link"
-                @click="setDrawerVisible({ val: false, time: 0 })"
-                dense
-                v-bind="attrs"
-                v-on="on"
-              >
-                <v-list-item-icon>
-                  <v-icon v-bind="attrs" v-on="on">{{ subItem.icon }}</v-icon>
-                </v-list-item-icon>
-                <v-list-item-content>
-                  <v-list-item-title>
-                    {{ $t(subItem.key) }}
-                  </v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </template>
-            <div v-html="$t('tooltip.' + subItem.key)"></div>
-          </v-tooltip>
+            <v-tooltip
+              v-if="subItem.link !== 'SPACER'"
+              top
+              :open-delay="openDelay"
+              :disabled="!tooltips"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-list-item
+                  :to="subItem.link"
+                  @click="setDrawerVisible({ val: false, time: 0 })"
+                  dense
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  <v-list-item-icon>
+                    <v-icon v-bind="attrs" v-on="on">{{ subItem.icon }}</v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      {{ $t(subItem.key) }}
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </template>
+              <div v-html="$t('tooltip.' + subItem.key)"></div>
+            </v-tooltip>
+          </div>
         </div>
       </v-list>
     </div>
@@ -81,6 +107,9 @@ export default {
     ...mapGetters('gui/tooltips', {
       tooltips: 'tooltips',
       openDelay: 'openDelay'
+    }),
+    ...mapGetters('keycloak', {
+      keycloakClientRoles: 'clientRoles'
     })
   },
 
